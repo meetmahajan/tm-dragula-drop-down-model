e9cd0d78715aff1b83a2431d03c3c4a9599449c1
meetmahajan <monoarchos@gmail.com>
2019-09-25 16:44:32 +0530

    Pipe created for sorting

--- src/app/drag-and-drop-table/drag-and-drop-table.module.ts
+++ src/app/drag-and-drop-table/drag-and-drop-table.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragAndDropTableComponent } from './drag-and-drop-table/drag-and-drop-table.component';
+import { OrderByPositionPipe } from './order-by-position.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [DragAndDropTableComponent],
+  declarations: [DragAndDropTableComponent, OrderByPositionPipe],
   exports:[DragAndDropTableComponent]
 })
 export class DragAndDropTableModule { }
---
+++ src/app/drag-and-drop-table/order-by-position.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+interface IPosition {
+    position: number;
+}
+
+@Pipe({
+  name: 'orderByPosition'
+})
+export class OrderByPositionPipe implements PipeTransform {
+
+  transform(array: Array<IPosition>): Array<IPosition> {
+    return array.sort((a,b) => a.position-b.position);
+  }
+
+}
