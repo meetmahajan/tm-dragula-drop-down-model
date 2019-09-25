d6d09350d9b71bbdf796fd666fd7eb4f028a45ae
meetmahajan <monoarchos@gmail.com>
2019-09-25 16:46:18 +0530

    Added into the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DragAndDropTableModule } from './drag-and-drop-table/drag-and-drop-table.module';
 
+// https://stackblitz.com/edit/angular-dragula-drag-drop-example
 
 @NgModule({
   declarations: [
--- src/app/drag-and-drop-table/drag-and-drop-table.module.ts
+++ src/app/drag-and-drop-table/drag-and-drop-table.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragAndDropTableComponent } from './drag-and-drop-table/drag-and-drop-table.component';
+import { DragulaModule } from 'ng2-dragula';
 import { OrderByPositionPipe } from './order-by-position.pipe';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DragulaModule.forRoot()
   ],
   declarations: [DragAndDropTableComponent, OrderByPositionPipe],
   exports:[DragAndDropTableComponent]
